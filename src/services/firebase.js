73bd4a5dcf5e9d35f06b91f69b65996498309eb5
Gorka Jesus <gorkajesusquesadavega@alumno.ieselrincon.es>
2025-03-26 18:04:37 +0000

    Firebase Hosting applied

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,15 +3,15 @@ import { getDatabase } from "firebase/database";
 // MY NEW APP
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCCJarlyGFTczph-WUGuLkzGzWY0_sSTOs",
-    authDomain: "sport-spaces-aa524.firebaseapp.com",
-    databaseURL: "https://sport-spaces-aa524-default-rtdb.europe-west1.firebasedatabase.app/",
-    projectId: "sport-spaces-aa524",
-    storageBucket: "sport-spaces-aa524.firebasestorage.app",
-    messagingSenderId: "293088433368",
-    appId: "1:293088433368:web:6761b5b990f15ff705f3ad",
-    measurementId: "G-89MWK8343S"
-  };
+  apiKey: "AIzaSyCCJarlyGFTczph-WUGuLkzGzWY0_sSTOs",
+  authDomain: "sport-spaces-aa524.firebaseapp.com",
+  databaseURL: "https://sport-spaces-aa524-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "sport-spaces-aa524",
+  storageBucket: "sport-spaces-aa524.firebasestorage.app",
+  messagingSenderId: "293088433368",
+  appId: "1:293088433368:web:6761b5b990f15ff705f3ad",
+  measurementId: "G-89MWK8343S"
+};
 
 
 
